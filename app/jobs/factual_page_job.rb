852d8069bdd81be20fd84b28819ea155b1cc7815
Jon Allured <jon.allured@gmail.com>
2017-02-10 16:01:15 -0600

    Simplify perform method
    This is mostly to fix a LineLength violation, but it actually reads
    nicely and is more accurate. Factual.new returns a Factual object, but
    then once you start calling `table` and `filters`, etc on it, it turns
    into a query object.

--- app/jobs/factual_page_job.rb
+++ app/jobs/factual_page_job.rb
@@ -1,12 +1,16 @@
 class FactualPageJob < ApplicationJob
   def perform
-    factual_galaries = client.table(table).filters(filters).page(page, per: 50).rows
+    factual_galaries = query.rows
     return unless factual_galaries.any?
     FactualPage.create payload: factual_galaries, table: table, page: page
     FactualPageJob.perform_later
   end
 
   private
+
+  def query
+    client.table(table).filters(filters).page(page, per: 50)
+  end
 
   def client
     @factual ||= Factual.new(ENV['FACTUAL_KEY'], ENV['FACTUAL_SECRET'])
