5e83bba9dfb5ebc50213e779231931427d3ac012
Jon Allured <jon.allured@gmail.com>
2017-02-16 13:43:21 -0600

    Update relationship names

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,16 +1,16 @@
 class Organization < ApplicationRecord
   has_many :locations
-  has_many :imports_organizations_names
-  has_many :imports_organizations_tags
-  has_many :tags, through: :imports_organizations_tags
+  has_many :import_organization_names
+  has_many :import_organization_tags
+  has_many :tags, through: :import_organization_tags
 
   has_paper_trail ignore: [:created_at, :updated_at]
 
-  def imports_organizations_tags_for(tag)
-    imports_organizations_tags.where(tag: tag)
+  def import_organization_tags_for(tag)
+    import_organization_tags.where(tag: tag)
   end
 
-  def imports_organizations_names_for(name)
-    imports_organizations_names.where(name: name)
+  def import_organization_names_for(name)
+    import_organization_names.where(name: name)
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
-  has_many :imports_organizations_tags
-  has_many :imports, through: :imports_organizations_tags
+  has_many :import_organization_tags
+  has_many :imports, through: :import_organization_tags
 
   validates :name, presence: true
 end
