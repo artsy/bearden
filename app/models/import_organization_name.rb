b0c36149ee55801e737bef6e156f1dbc49cf7d50
Jon Allured <jon.allured@gmail.com>
2017-02-16 13:28:38 -0600

    Order things alpha when possible

--- app/models/import_organization_name.rb
+++ app/models/import_organization_name.rb
@@ -1,9 +1,9 @@
 class ImportOrganizationName < ApplicationRecord
-  belongs_to :name
   belongs_to :import
+  belongs_to :name
   belongs_to :organization
 
-  validates_uniqueness_of :organization, scope: [:name, :import]
+  validates_uniqueness_of :organization, scope: [:import, :name]
 
   self.table_name = 'imports_organizations_names'
 end
--- app/models/import_organization_tag.rb
+++ app/models/import_organization_tag.rb
@@ -1,9 +1,9 @@
 class ImportOrganizationTag < ApplicationRecord
-  belongs_to :tag
   belongs_to :import
   belongs_to :organization
+  belongs_to :tag
 
-  validates_uniqueness_of :organization_id, scope: [:tag, :import]
+  validates_uniqueness_of :organization_id, scope: [:import, :tag]
 
   self.table_name = 'imports_organizations_tags'
 end
