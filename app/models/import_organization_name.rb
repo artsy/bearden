5f5ecede862fd0c98af26e3dfb254619c7210e4d
Jon Allured <jon.allured@gmail.com>
2017-02-16 13:24:30 -0600

    Fix name of join class
    The precedent I'm following here is that tables are plural and model
    classes are singular.

--- app/models/import_organization_name.rb
+++ app/models/import_organization_name.rb
@@ -1,7 +1,9 @@
-class ImportsOrganizationsName < ActiveRecord
+class ImportOrganizationName < ApplicationRecord
   belongs_to :name
   belongs_to :import
   belongs_to :organization
 
   validates_uniqueness_of :organization, scope: [:name, :import]
+
+  self.table_name = 'imports_organizations_names'
 end
--- app/models/import_organization_tag.rb
+++ app/models/import_organization_tag.rb
@@ -1,7 +1,9 @@
-class ImportsOrganizationsTag < ApplicationRecord
+class ImportOrganizationTag < ApplicationRecord
   belongs_to :tag
   belongs_to :import
   belongs_to :organization
 
   validates_uniqueness_of :organization_id, scope: [:tag, :import]
+
+  self.table_name = 'imports_organizations_tags'
 end
