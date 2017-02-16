605fe113cb2366c7902928494d963e90259aec66
Jon Allured <jon.allured@gmail.com>
2017-02-16 13:50:34 -0600

    Update specs

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -11,12 +11,12 @@ RSpec.describe Organization, type: :model do
     org1 = Fabricate :organization
     org2 = Fabricate :organization
 
-    org1.imports_organizations_tags.create! tag: shared_tag, import: import_a
-    org1.imports_organizations_tags.create! tag: shared_tag, import: import_c
-    org2.imports_organizations_tags.create! tag: shared_tag, import: import_b
+    org1.import_organization_tags.create! tag: shared_tag, import: import_a
+    org1.import_organization_tags.create! tag: shared_tag, import: import_c
+    org2.import_organization_tags.create! tag: shared_tag, import: import_b
 
-    expect(org1.imports_organizations_tags_for(shared_tag).count).to eql 2
-    expect(org2.imports_organizations_tags_for(shared_tag).count).to eql 1
+    expect(org1.import_organization_tags_for(shared_tag).count).to eql 2
+    expect(org2.import_organization_tags_for(shared_tag).count).to eql 1
   end
 
   it 'has multiple locations' do
