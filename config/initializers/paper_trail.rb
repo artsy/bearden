3a1de4af7157e913269804485223ee4c95f278fe
Jon Allured <jon.allured@gmail.com>
2017-02-21 10:47:26 -0600

    Add wrapper for PaperTrail
    This really seems like the only sane way to set whodunnit otherwise
    you're going to leak into other actions accidentally.

--- config/initializers/paper_trail.rb
+++ config/initializers/paper_trail.rb
@@ -1,1 +1,10 @@
 PaperTrail.config.track_associations = false
+
+module PaperTrail
+  def self.with_actor(actor)
+    current_actor = self.actor
+    self.whodunnit = actor
+    yield
+    self.whodunnit = current_actor
+  end
+end
