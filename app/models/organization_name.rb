b6873f432200c9da3aa30c71e3ebad7d17ccf7ef
Jon Allured <jon.allured@gmail.com>
2017-02-22 11:20:03 -0600

    Add PaperTrail to relevant models

--- app/models/organization_name.rb
+++ app/models/organization_name.rb
@@ -3,4 +3,6 @@ class OrganizationName < ApplicationRecord
 
   validates_presence_of :content
   validates_uniqueness_of :organization, scope: [:import, :name]
+
+  has_paper_trail ignore: [:created_at, :updated_at]
 end
