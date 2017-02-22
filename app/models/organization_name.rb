65e3fbb6c9d01bb676ff145f342785823ca26076
Jon Allured <jon.allured@gmail.com>
2017-02-22 11:50:54 -0600

    Fix broken validation
    As @gnilekaw correctly pointed out, this validation was broken and also
    didn't really validate the thing we were trying to validate - that a
    particular name was unique to an Organization. Shamelessly stolen from
    his PR feedback. :bowtie:

--- app/models/organization_name.rb
+++ app/models/organization_name.rb
@@ -2,7 +2,7 @@ class OrganizationName < ApplicationRecord
   belongs_to :organization
 
   validates_presence_of :content
-  validates_uniqueness_of :organization, scope: [:import, :name]
+  validates_uniqueness_of :content, scope: :organization
 
   has_paper_trail ignore: [:created_at, :updated_at]
 end
