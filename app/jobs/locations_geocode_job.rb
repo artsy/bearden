afee62003bbd64a208593bf542d80c73ec2a2c61
Lance W <lwakeling@gmail.com>
2017-03-01 10:26:30 -0500

    Adds Geocoder gem and a job to process Locations that are not geocoded

---
+++ app/jobs/locations_geocode_job.rb
@@ -0,0 +1,23 @@
+class LocationsGeocodeJob < ApplicationJob
+  def perform(location_id)
+    location = Location.find_by id: location_id
+    return unless location&.geocodable?
+
+    begin
+      coordinates = Geocoder.coordinates(location.content)
+
+      unless coordinates
+        logger.warn "Could not geocode Location: #{location.id}"
+        return
+      end
+
+      location.update_attributes latitude: coordinates[0], longitude: coordinates[1]
+
+      next_location = Location.where(latitude: nil).or(Location.where(longitude: nil)).first
+      self.class.perform_later next_location.id if next_location
+
+    rescue Geocoder::OverQueryLimitError
+      logger.warn "Geocoder is over its query limit"
+    end
+  end
+end
