4752c21b77df2df6e3cc019b54cb53c97f18d914
Lance W <lwakeling@gmail.com>
2017-03-06 16:10:13 -0500

    Adds Rankable to email and phone number

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,5 +2,5 @@ class Email < ApplicationRecord
   belongs_to :organization
   validates :content, presence: true
   has_paper_trail ignore: [:created_at, :updated_at]
-  # TODO: include Rankable
+  include Rankable
 end
--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -2,5 +2,5 @@ class PhoneNumber < ApplicationRecord
   belongs_to :organization
   validates :content, presence: true
   has_paper_trail ignore: [:created_at, :updated_at]
-  # TODO: include Rankable
+  include Rankable
 end
