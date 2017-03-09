926e5d3675c9150436268d7502c484f04ffb0a6a
Lance W <lwakeling@gmail.com>
2017-03-09 17:09:51 -0500

    Website should be unique

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,6 +1,6 @@
 class Website < ApplicationRecord
   belongs_to :organization
-  validates :content, presence: true
+  validates :content, presence: true, uniqueness: true
   has_paper_trail ignore: [:created_at, :updated_at]
   include Rankable
 end
