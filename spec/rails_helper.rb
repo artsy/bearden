689003db37557d57cfa5a441473b173866983dfe
Lance W <lwakeling@gmail.com>
2017-03-10 15:23:04 -0500

    Sets use_transactional_fixtures to false.
    Using DatabaseCleaner strategy of truncation, may be slower than
    transaction. Unfortunately, Rspec uses transaction by default, which
    conflics with RawInputChanges's use of transaction.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -19,7 +19,16 @@ RSpec.configure do |config|
   end
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
+  config.use_transactional_fixtures = false
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
