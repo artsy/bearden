6a61502787493403b6f0b3ea2212389555d1d529
Lance W <lwakeling@gmail.com>
2017-03-10 11:39:36 -0500

    Website should validate for presence of Organization

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,5 +1,6 @@
 class Website < ApplicationRecord
   belongs_to :organization
+  validates :organization, presence: true
   validates :content, presence: true, uniqueness: true
   has_paper_trail ignore: [:created_at, :updated_at]
   include Rankable
