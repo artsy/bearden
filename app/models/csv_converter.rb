f5c32c09639f7f0683de2c20e3d7395870ed00a0
Jon Allured <jon.allured@gmail.com>
2017-03-14 16:42:53 -0500

    Export email and phone number
    This commit finishes off the work to support emails and phone numbers by
    allowing it to be exported. It's another mostly copy/paste commit, but I
    once again found myself disabling cops. Insert ideas here!

--- app/models/csv_converter.rb
+++ app/models/csv_converter.rb
@@ -1,15 +1,19 @@
 class CsvConverter
+  # rubocop:disable Metrics/MethodLength
   def self.headers
     %w(
       bearden_id
+      email
       latitude
       location
       longitude
       organization_name
+      phone_number
       tag_names
       website
     )
   end
+  # rubocop:enable Metrics/MethodLength
 
   def self.convert(input)
     new(input).convert
@@ -19,15 +23,19 @@ class CsvConverter
     @input = input
   end
 
+  # rubocop:disable Metrics/MethodLength
   def convert
     [
       @input[:bearden_id],
+      @input[:email],
       @input[:latitude],
       @input[:location],
       @input[:longitude],
       @input[:organization_name],
+      @input[:phone_number],
       @input[:tag_names],
       @input[:website]
     ]
   end
+  # rubocop:enable Metrics/MethodLength
 end
---
+++ spec/fabricators/phone_number_fabricator.rb
@@ -0,0 +1,3 @@
+Fabricator :phone_number do
+  content '1-800-555-7777'
+end
