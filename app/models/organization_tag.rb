aeea636565ddda028971c325a972e9aae81c2204
Jon Allured <jon.allured@gmail.com>
2017-03-14 15:57:22 -0500

    Support tags in CSV import rake task
    This commit adds PaperTrail to `OrganizationTag` and then updates the
    collaborator classes to the import rake task so that they pay attention
    to a column I'm calling `tag_names`. The idea here is to make this be a
    comma-separated list of tag names.
    
    The actual work of tagging, I've pushed down to the `OrganizationTag`
    class. It seemed like a good place for the knowledge of what to do in
    the various combinations that we could get ourselves into.
    
    Note: I have disabled a RuboCop violation - you mad? I feel bad about
    this, but not enough to refactor. I know that this part of the code is
    going to have conflicts with #65 and once I get to #73 and #74, I will
    be adding even more here.
    
    I guess my point is that I'd like to feel the pain a little longer
    before I figure out what I'd like to do here. And I reserve the right to
    continue ignoring, haha! Unless someone feels strongly or has a better
    idea, of course. :wink:

--- app/models/organization_tag.rb
+++ app/models/organization_tag.rb
@@ -2,4 +2,14 @@ class OrganizationTag < ApplicationRecord
   belongs_to :organization
   belongs_to :tag
   validates_uniqueness_of :tag, scope: :organization
+
+  has_paper_trail ignore: [:created_at, :updated_at]
+
+  def self.apply(tag_names, organization)
+    tag_names.each do |tag_name|
+      tag = Tag.find_or_create_by name: tag_name
+      next if organization.tags.exists?(name: tag.name)
+      organization.organization_tags.create!(tag: tag)
+    end
+  end
 end
--- spec/models/organization_tag_spec.rb
+++ spec/models/organization_tag_spec.rb
@@ -13,4 +13,45 @@ describe OrganizationTag do
       end.to raise_error ActiveRecord::RecordInvalid
     end
   end
+
+  describe '.apply' do
+    context 'with a new tag' do
+      it 'creates that tag and applies to the organization' do
+        organization = Fabricate :organization
+        OrganizationTag.apply ['new_tag'], organization
+        expect(organization.tags.count).to eq 1
+        expect(organization.tags.first.name).to eq 'new_tag'
+      end
+    end
+
+    context 'with an existing tag and an organzation without that tag' do
+      it 'applies that tag to that organzation' do
+        organization = Fabricate :organization
+        tag = Fabricate :tag
+        OrganizationTag.apply [tag.name], organization
+        expect(Tag.count).to eq 1
+        expect(organization.tags.count).to eq 1
+      end
+    end
+
+    context 'with an existing tag and an organzation with that tag' do
+      it 'ignores that tag for that organzation' do
+        organization = Fabricate :organization
+        tag = Fabricate :tag
+        Fabricate :organization_tag, organization: organization, tag: tag
+        OrganizationTag.apply [tag.name], organization
+        expect(OrganizationTag.count).to eq 1
+        expect(Tag.count).to eq 1
+        expect(organization.tags.count).to eq 1
+      end
+    end
+
+    context 'with a few tags' do
+      it 'applies each of them' do
+        organization = Fabricate :organization
+        OrganizationTag.apply %w(new_tag another last_tag), organization
+        expect(organization.tags.count).to eq 3
+      end
+    end
+  end
 end
