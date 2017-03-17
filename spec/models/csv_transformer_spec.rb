a68cfcb958c959732353eecc4a94f0cd600302e8
Jon Allured <jon.allured@gmail.com>
2017-03-17 12:45:18 -0500

    Add a few more missing tests
    While talking this spec over with @gnilkaw, we realized that there were
    still more cases that weren't covered. I *think* we have them all now.

--- spec/models/csv_transformer_spec.rb
+++ spec/models/csv_transformer_spec.rb
@@ -20,7 +20,7 @@ describe CsvTransformer do
       end
     end
 
-    context 'with some fields' do
+    context 'with partial fields' do
       it 'returns only those fields that are not empty' do
         data = {
           email: 'info@example.com',
@@ -48,8 +48,36 @@ describe CsvTransformer do
       end
     end
 
-    context 'with all fields' do
-      it 'returns attrs for an Organization' do
+    context 'with sparse fields' do
+      it 'returns only those fields that are not empty' do
+        data = {
+          email: '',
+          location: '123 Main Street, New York, NY 10001',
+          latitude: '47.5543105',
+          longitude: '7.598538899999999',
+          organization_name: '',
+          phone_number: '',
+          tag_names: '',
+          website: 'http://example.com'
+        }
+        raw_input = Fabricate :raw_input, data: data
+        attrs = CsvTransformer.transform raw_input
+        expected = {
+          location: {
+            content: data[:location],
+            latitude: data[:latitude],
+            longitude: data[:longitude]
+          },
+          website: {
+            content: data[:website]
+          }
+        }
+        expect(attrs).to eq(expected)
+      end
+    end
+
+    context 'with complete fields' do
+      it "returns all attrs for an Organization and it's relationships" do
         data = {
           email: 'info@example.com',
           location: '123 Main Street, New York, NY 10001',
@@ -78,6 +106,32 @@ describe CsvTransformer do
             content: data[:phone_number]
           },
           tag_names: %w(design modern),
+          website: {
+            content: data[:website]
+          }
+        }
+        expect(attrs).to eq(expected)
+      end
+    end
+
+    context 'with bonus fields' do
+      it 'returns a hash that ignores those fields' do
+        data = {
+          emmmmail: 'info@example.com',
+          omglol: 'hahaha',
+          organization_name: 'Best Gallery',
+          phone_number: '1-800-123-4567',
+          website: 'http://example.com'
+        }
+        raw_input = Fabricate :raw_input, data: data
+        attrs = CsvTransformer.transform raw_input
+        expected = {
+          organization_name: {
+            content: data[:organization_name]
+          },
+          phone_number: {
+            content: data[:phone_number]
+          },
           website: {
             content: data[:website]
           }
