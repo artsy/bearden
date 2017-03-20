bf9689adbcd19997b428cf9062c46f8556b15420
Lance W <lwakeling@gmail.com>
2017-03-20 10:21:30 -0400

    Remove space from Import name

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 Source.create name: 'Factual', rank: 1
-Source.create name: 'Human Outsourcer', rank: 2
+Source.create name: 'HumanOutsourcer', rank: 2
 Source.create name: 'Geocoder', rank: 3
 Source.create name: 'Burden', rank: 4
 
