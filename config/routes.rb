81d25197553c6ad152e11c97a7c4a359a89e274b
Jon Allured <jon.allured@gmail.com>
2017-03-21 11:34:56 -0500

    Importer views list of tags
    The idea here is that an importer working on their CSV file might want
    to view the list of tags to know what's allowed. They'd probably use
    this when doing some find/replace work.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
   # see config/initializers/sidekiq.rb for security details
   mount Sidekiq::Web, at: '/sidekiq'
+
+  resources :tags, only: :index
 end
