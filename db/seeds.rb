8b367473fe7a90ac771e5a99f65edde83bce0fa6
Jon Allured <jon.allured@gmail.com>
2017-03-21 13:44:26 -0500

    Simplify seed data
    Per feedback from @gnilekaw.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,26 +3,30 @@ Source.create name: 'HumanOutsourcer', rank: 2
 Source.create name: 'Geocoder', rank: 3
 Source.create name: 'Burden', rank: 4
 
-Tag.create name: 'ancient art / artifacts / antiquities'
-Tag.create name: 'antique shop'
-Tag.create name: 'art'
-Tag.create name: 'art merchandise and multiples / ephemera'
-Tag.create name: 'artist'
-Tag.create name: 'artist estates / foundations'
-Tag.create name: 'artist studios'
-Tag.create name: 'auction houses'
-Tag.create name: 'benefit auctions'
-Tag.create name: 'biennials / triennials'
-Tag.create name: 'commercial auctions'
-Tag.create name: 'dealer'
-Tag.create name: 'design / decorative arts'
-Tag.create name: 'fairs'
-Tag.create name: 'gallery'
-Tag.create name: 'jewelry'
-Tag.create name: 'museum'
-Tag.create name: 'nonprofit organizations'
-Tag.create name: 'other'
-Tag.create name: 'print publishers and print dealers'
-Tag.create name: 'private collections'
-Tag.create name: 'publications / publishers / archives'
-Tag.create name: 'university museums / educational institutions'
+[
+  'ancient art / artifacts / antiquities',
+  'antique shop',
+  'art',
+  'art merchandise and multiples / ephemera',
+  'artist',
+  'artist estates / foundations',
+  'artist studios',
+  'auction houses',
+  'benefit auctions',
+  'biennials / triennials',
+  'commercial auctions',
+  'dealer',
+  'design / decorative arts',
+  'fairs',
+  'gallery',
+  'jewelry',
+  'museum',
+  'nonprofit organizations',
+  'other',
+  'print publishers and print dealers',
+  'private collections',
+  'publications / publishers / archives',
+  'university museums / educational institutions'
+].each do |name|
+  Tag.create name: name
+end
