ce47e5676929476f44504ceea25a9e73b09208ae
Jon Allured <jon.allured@gmail.com>
2017-03-21 15:54:11 -0500

    Use inline adapter for feature specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,6 +21,13 @@ RSpec.configure do |config|
     PaperTrail.whodunnit = 'Test User'
   end
 
+  config.around(:each, type: :feature) do |example|
+    original_adapter = ActiveJob::Base.queue_adapter
+    ActiveJob::Base.queue_adapter = :inline
+    example.run
+    ActiveJob::Base.queue_adapter = original_adapter
+  end
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
