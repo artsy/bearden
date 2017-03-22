4c019bd233d109da24a55dde4615d6263ab88dc4
Jon Allured <jon.allured@gmail.com>
2017-03-22 11:41:28 -0500

    Force SSL
    Ripped right from the docs:
    
    http://api.rubyonrails.org/classes/ActionController/ForceSSL/ClassMethods.html
    
    My other thought was to use an ENV var, but I decided to let that type
    of requirement fall out - my guess is that it won't.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  force_ssl if: :ssl_configured?
+
+  private
+
+  def ssl_configured?
+    !Rails.env.development?
+  end
 end
