4cad9bab29905f5df3f0177c2e1de1026502e147
Jon Allured <jon.allured@gmail.com>
2017-03-22 09:45:38 -0500

    Add basic header with navigation
    Shamelessly stolen from another project where I did a basic admin UI.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def nav_class(element)
+    class_string = 'nav-link'
+
+    navigation_elements = {
+      tags: tags_path
+    }
+
+    class_string << ' active' if navigation_elements[element] == request.path
+    class_string
+  end
 end
