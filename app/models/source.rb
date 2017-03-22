4bc1b0d7f472ce2aabaad67e99bfe823d72e3d55
Jon Allured <jon.allured@gmail.com>
2017-03-22 15:50:35 -0500

    Add more granularity to source rankings
    Prior to this commit rank was all or none - either one source was better
    than another or not. What was asked for was a way for sources to be
    ranked for each individual field for an organization.
    
    The first thing that came to mind was to go from one column for rank to
    one column per field for rank and that's what this commit does.

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,6 +1,22 @@
 class Source < ApplicationRecord
+  class UnknownRankable < StandardError; end
   has_many :imports
   validates :name, presence: true
-  validates_presence_of :rank
-  validates_uniqueness_of :rank
+
+  validates :email_rank, presence: true, uniqueness: true
+  validates :location_rank, presence: true, uniqueness: true
+  validates :organization_name_rank, presence: true, uniqueness: true
+  validates :phone_number_rank, presence: true, uniqueness: true
+  validates :website_rank, presence: true, uniqueness: true
+
+  def rank_for(rankable)
+    case rankable
+    when Email then email_rank
+    when Location then location_rank
+    when OrganizationName then organization_name_rank
+    when PhoneNumber then phone_number_rank
+    when Website then website_rank
+    else raise UnknownRankable
+    end
+  end
 end
--- spec/fabricators/source_fabricator.rb
+++ spec/fabricators/source_fabricator.rb
@@ -1,4 +1,8 @@
 Fabricator :source do
   name { Fabricate.sequence(:name) { |i| "Source ##{i}" } }
-  rank { Fabricate.sequence(:rank) }
+  email_rank { Fabricate.sequence(:email_rank) }
+  location_rank { Fabricate.sequence(:location_rank) }
+  organization_name_rank { Fabricate.sequence(:organization_name_rank) }
+  phone_number_rank { Fabricate.sequence(:phone_number_rank) }
+  website_rank { Fabricate.sequence(:website_rank) }
 end
