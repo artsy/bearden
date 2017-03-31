f9f2e11d69bdbc8781fda72a2cd2b2b379f7006f
Jon Allured <jon.allured@gmail.com>
2017-03-31 08:41:16 -0500

    Add Source rankings table to list page
    The idea here is that when you want to see how the ranks of sources are
    going to resolve, it's helpful to have this in a table.
    
    closes #133

--- spec/features/list_sources_spec.rb
+++ spec/features/list_sources_spec.rb
@@ -2,8 +2,70 @@ require 'rails_helper'
 
 feature 'List Sources' do
   scenario 'Importer views list of sources' do
-    sources = Fabricate.times 3, :source
+    Fabricate(
+      :source,
+      name: 'Source 1',
+      email_rank: 1,
+      location_rank: 2,
+      organization_name_rank: 1,
+      phone_number_rank: 1,
+      website_rank: 3
+    )
+
+    Fabricate(
+      :source,
+      name: 'Source 2',
+      email_rank: 3,
+      location_rank: 1,
+      organization_name_rank: 2,
+      phone_number_rank: 2,
+      website_rank: 1
+    )
+
+    Fabricate(
+      :source,
+      name: 'Source 3',
+      email_rank: 2,
+      location_rank: 3,
+      organization_name_rank: 3,
+      phone_number_rank: 3,
+      website_rank: 2
+    )
+
     visit '/sources'
-    sources.each { |source| expect(page).to have_text source.name }
+
+    Source.all.each { |source| expect(page).to have_text source.name }
+
+    (first_row, second_row, third_row) = page.all('tbody tr').to_a
+
+    expect(first_row.all('td').map(&:text)).to eq(
+      [
+        '1. Source 1',
+        '1. Source 2',
+        '1. Source 1',
+        '1. Source 1',
+        '1. Source 2'
+      ]
+    )
+
+    expect(second_row.all('td').map(&:text)).to eq(
+      [
+        '2. Source 3',
+        '2. Source 1',
+        '2. Source 2',
+        '2. Source 2',
+        '2. Source 3'
+      ]
+    )
+
+    expect(third_row.all('td').map(&:text)).to eq(
+      [
+        '3. Source 2',
+        '3. Source 3',
+        '3. Source 3',
+        '3. Source 3',
+        '3. Source 1'
+      ]
+    )
   end
 end
