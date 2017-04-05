66a4db4d99eeb77d01adee2a1dc1890ea30c27ed
Lance W <lwakeling@gmail.com>
2017-04-05 16:53:24 -0400

    Add 404 for forbiden users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,30 @@ class ApplicationController < ArtsyAuth::ApplicationController
   protect_from_forgery with: :exception
   force_ssl if: :ssl_configured?
 
+  ALLOWED_ROLES = ['admin'].freeze
+
   def authorized_artsy_token?(token)
+    roles = user_roles(token)
+    success_or_die(roles)
+  end
+
+  def user_roles(token)
     secret = Rails.application.secrets.artsy_internal_secret
     decoded_token, _headers = JWT.decode(token, secret)
-    decoded_token['roles'].include? 'admin'
+    decoded_token['roles'].split(',')
   end
 
   private
 
   def ssl_configured?
     !Rails.env.development?
+  end
+
+  def role_permitted?(roles)
+    roles.any? { |role| ALLOWED_ROLES.member? role }
+  end
+
+  def success_or_die(roles)
+    role_permitted?(roles) || render(plain: '404 Not Found', status: 404)
   end
 end
