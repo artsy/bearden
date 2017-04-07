344a08d16efe1aa5238b3a426dbc7fa927daeebd
Jon Allured <jon.allured@gmail.com>
2017-04-07 08:44:38 -0500

    Add Sync model
    I don't usually write commits like this - it just creates the model and
    doesn't do anything with it, but with all the churn on this stuff, I
    felt like I had to get something committed.

---
+++ spec/models/sync_micro_machine_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe SyncMicroMachine do
+  describe '.in_progress_states' do
+    it 'returns valid states except finished' do
+      in_progress_states = SyncMicroMachine.in_progress_states
+      expect(in_progress_states).to eq %w(
+        unstarted
+        starting
+        exporting
+        copying
+        finalizing
+      )
+    end
+  end
+
+  describe '.valid_states' do
+    it 'returns the valid states' do
+      valid_states = SyncMicroMachine.valid_states
+      expect(valid_states).to eq %w(
+        unstarted
+        starting
+        exporting
+        copying
+        finalizing
+        finished
+      )
+    end
+  end
+end
