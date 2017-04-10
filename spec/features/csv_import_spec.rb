82e87944ab5d94f6bd8694f4f8fe1794b464d76c
Lance W <lwakeling@gmail.com>
2017-04-10 16:49:47 -0400

    Fixes failing test from CarrierWave :dagger: :sob:

--- spec/features/csv_import_spec.rb
+++ spec/features/csv_import_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 feature 'CSV Import' do
   scenario 'Import one complete gallery' do
+    csv_file = 'spec/fixtures/one_complete_gallery.csv'
+
+    allow_any_instance_of(S3CsvUploader).to receive(:store!)
+    allow_any_instance_of(Fog::Storage::AWS::GetObjectUrl)
+      .to receive(:get_object_url).and_return(csv_file)
+
     Fabricate :tag, name: 'design'
     Fabricate :tag, name: 'modern'
     source = Fabricate :source, name: 'Factual'
@@ -9,7 +15,7 @@ feature 'CSV Import' do
     visit '/imports/new'
 
     select source.name, from: 'Source'
-    attach_file('import_csv', 'spec/fixtures/one_complete_gallery.csv')
+    attach_file('import_csv', csv_file)
     fill_in 'Description', with: 'Monthly Data Import'
 
     click_button 'Create'
