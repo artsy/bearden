09a15945f48438c333dba1480a50173db745574e
Lance W <gnilekaw@protonmail.com>
2017-06-30 13:37:21 -0400

    :eyeglasses: Update spec for geocoder_response

--- spec/jobs/geocode_location_job_spec.rb
+++ spec/jobs/geocode_location_job_spec.rb
@@ -9,12 +9,13 @@ describe GeocodeLocationJob do
       allow(Geocoder).to receive(:search)
     end
 
-    it 'stops raises an error when OverQueryLimitError is raised' do
+    it 'raises an error when OverQueryLimitError is raised' do
       expect(Geocoder).to receive(:search)
         .and_raise(Geocoder::OverQueryLimitError)
       org = Fabricate :organization
       org.locations.create(
-        content: 'New York City', latitude: 1234, longitude: nil
+        content: 'New York City',
+        geocode_response: nil
       )
 
       perform_enqueued_jobs do
@@ -26,8 +27,14 @@ describe GeocodeLocationJob do
 
     it 'enqueues one job after another, after another, after another...' do
       org = Fabricate :organization
-      org.locations.create content: 'Berlin, Germany'
-      org.locations.create content: '401 Broadway, New York City'
+      org.locations.create(
+        content: 'Berlin, Germany',
+        geocode_response: nil
+      )
+      org.locations.create(
+        content: '401 Broadway, New York City',
+        geocode_response: nil
+      )
 
       perform_enqueued_jobs do
         GeocodeLocationJob.perform_later
@@ -39,7 +46,7 @@ describe GeocodeLocationJob do
       org = Fabricate :organization
       org.locations.create content: 'Berlin, Germany'
       org.locations.create(
-        content: 'New York City', latitude: 1234, longitude: 5678
+        content: 'New York City', geocode_response: { foo: 'bar' }.to_json
       )
 
       perform_enqueued_jobs do
@@ -50,9 +57,9 @@ describe GeocodeLocationJob do
 
     it 'only does jobs for non-geocoded Locations' do
       org = Fabricate :organization
-      org.locations.create content: 'Berlin, Germany'
+      org.locations.create content: 'Berlin, Germany', geocode_response: nil
       org.locations.create(
-        content: 'New York City', latitude: 1234, longitude: 5678
+        content: 'New York City', geocode_response: { foo: 'bar' }.to_json
       )
 
       perform_enqueued_jobs do
@@ -66,31 +73,34 @@ describe GeocodeLocationJob do
       fallback_coordinates = GeocodeLocationJob::FALLBACK_COORDINATES
 
       allow(Geocoder).to receive(:search)
-        .and_return(nil)
+        .and_return([])
 
       org = Fabricate :organization
-      org.locations.create content: 'Nil Town, USA'
+      org.locations.create content: 'Nil Town, USA', geocode_response: nil
 
       perform_enqueued_jobs do
         GeocodeLocationJob.perform_later
       end
 
-      expect(org.locations.first.latitude).to eql fallback_coordinates[0]
-      expect(org.locations.first.longitude).to eql fallback_coordinates[1]
-      expect(org.locations.first.country).to eql nil
-      expect(org.locations.first.city).to eql nil
+      expect(org.locations.first.latitude).to eq fallback_coordinates[0]
+      expect(org.locations.first.longitude).to eq fallback_coordinates[1]
+      expect(org.locations.first.country).to eq nil
+      expect(org.locations.first.city).to eq nil
+
+      error = 'GeocodeLocationJob::NoResultsFound'
+      expect(org.locations.first.geocode_response).to eq error
     end
 
     it 'records an Import and Source for all records' do
       organization = Fabricate :organization
-      Fabricate :location, latitude: nil, organization: organization
-      Fabricate :location, latitude: nil, organization: organization
+      Fabricate :location, geocode_response: nil, organization: organization
+      Fabricate :location, geocode_response: nil, organization: organization
 
       perform_enqueued_jobs do
         GeocodeLocationJob.perform_later
 
         assert_performed_jobs 3
-        expect(Location.count).to eql 2
+        expect(Location.count).to eq 2
 
         Location.all.each do |location|
           actor = location.versions.last.actor
