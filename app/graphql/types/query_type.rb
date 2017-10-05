fbec57df77b8b8a5f2d8a4ebb3aff4bc9eb4d47f
dblock <dblock@dblock.org>
2017-10-05 14:04:33 -0400

    Search always returns something.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,7 +2,7 @@ Types::QueryType = GraphQL::ObjectType.define do
   name 'Query'
   description 'The query root of this schema.'
 
-  field :search, types[Types::OrganizationType] do
+  field :search, !types[Types::OrganizationType] do
     argument :term, !types.String
     argument :first, types.Int, default_value: 20
     description 'Find organizations by name.'
