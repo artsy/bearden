8e339c2808c9a621da94df871fd021ceaa3763b9
dblock <dblock@dblock.org>
2017-10-05 13:49:55 -0400

    Minor refactor to extract parameters.

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,14 +1,31 @@
 class GraphqlController < ApiController
   def execute
-    variables = params[:variables]
-    variables = JSON.parse(variables) if variables && variables.is_a?(String)
-    variables ||= {}
     result = BeardenSchema.execute(
-      params[:query],
+      query,
       variables: variables,
-      context: {},
-      operation_name: params[:operationName]
+      context: context,
+      operation_name: operation_name
     )
     render json: result
+  end
+
+  private
+
+  def query
+    params[:query]
+  end
+
+  def operation_name
+    params[:operationName]
+  end
+
+  def context
+    {}
+  end
+
+  def variables
+    variables = params[:variables]
+    variables = JSON.parse(variables) if variables && variables.is_a?(String)
+    variables || {}
   end
 end
